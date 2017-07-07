ccbaa8b21b6e3662ce59f6140d3462bb675baa22
mmorandini <emmemorandini@gmail.com>
2017-07-07 01:29:55 +0100

    added animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
   const maxWidth = document.body.clientWidth;
   const maxHeight = document.body.clientHeight; 
 
-  for (var i = 500; i >= 0; i--) {
+  for (var i = 100; i >= 0; i--) {
     const bug = document.createElement('div');
     document.body.appendChild(bug);
     $(bug).addClass("bug");
-    $(bug).width('2px');
-    $(bug).height('1px');
+    $(bug).width('4px');
+    $(bug).height('2px');
     $(bug).css('position','absolute');
     bugs.push(bug);
   }
@@ -27,14 +27,18 @@ $(document).ready(function(){
     };
   }
 
-  setInterval(moveBugs(), 500);
+  window.setInterval(animateBugs, 500);
 
-  function moveBugs(){
-    for (var i = bugs.length - 1; i >= 0; i--) {
-    let BUG = bugs[i]
-    $(BUG).animate({top: Math.round(maxHeight * Math.random()), left: Math.round(maxWidth * Math.random())});  
-    }
-  };
+  function animateBugs(){
+    
+      for (var i = bugs.length - 1; i >= 0; i--) {
+        let BUG = bugs[i]
+        $(BUG).animate({top: Math.round(maxHeight * Math.random()), left: Math.round(maxWidth * Math.random())});  
+      }
+    
+  }
+
+  
   
   console.log(document.body);
 });
